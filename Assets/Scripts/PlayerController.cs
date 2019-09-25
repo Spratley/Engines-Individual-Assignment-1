e5f23e122422aa0b74d7ba1da550f93ff6a63a59
Jacob Pratley <pratleyjacob@gmail.com>
2019-09-25 17:08:39 -0400

    Initial Push, done in class

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float runSpeed;
+    public float lookSpeed;
+    public float reticleSpeed;
+
+    private Vector3 cameraEulers;
+
+    private GameObject reticle;
+    private float reticleDist;
+
+    private void Start()
+    {
+        reticle = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        reticle.GetComponent<MeshRenderer>().material.color = Color.red;
+        reticle.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        UpdatePlayerMovement();
+
+        UpdateReticle();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject cubieboy = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cubieboy.GetComponent<MeshRenderer>().material.color = Color.blue;
+            cubieboy.transform.position = reticle.transform.position;
+            cubieboy.tag = "Saveable";
+        }
+    }
+
+    void UpdatePlayerMovement()
+    {
+        float moveSpeed = speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveSpeed = runSpeed;
+
+        transform.position += Quaternion.AngleAxis(cameraEulers.y, Vector3.up) * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed * Time.deltaTime;
+
+        cameraEulers += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * lookSpeed;
+        Camera.main.transform.rotation = Quaternion.Euler(cameraEulers);
+    }
+
+    void UpdateReticle()
+    {
+        reticleDist += Input.mouseScrollDelta.y * reticleSpeed;
+        if (reticleDist < 1)
+            reticleDist = 1;
+
+        reticle.transform.position = Camera.main.transform.position + Camera.main.transform.forward * reticleDist;
+    }
+}
---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+   
+public class SaveLoad : MonoBehaviour
+{
+    const string DLL_NAME = "SaveLoad";
+
+    [DllImport(DLL_NAME)]
+
+    public static extern int Save(Transform input);
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            
+            Debug.Log(Save(transform));
+            //GameObject[] objectsToSave = GameObject.FindGameObjectsWithTag("Saveable");
+            //foreach (GameObject go in objectsToSave)
+            //    if (!Save())
+            //        Debug.Log("Failed to save " + go.name);
+        }
+    }
+}
